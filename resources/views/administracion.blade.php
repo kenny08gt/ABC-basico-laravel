17421e1cdf050753715390e61b29f9d41c3fd07b
Alan <alan220193@gmail.com>
2016-08-07 18:23:06 +0000

    parallax agregado

---
+++ resources/views/administracion.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h3 class="heading">Administración</h3>
+ @include('common.errors')
+ 
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,45 +2,41 @@
 <html>
     <head>
         <title>Laravel</title>
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+  <!-- Compiled and minified JavaScript -->
+        
+          
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
     </head>
     <body>
         @include('common.errors')
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
-        </div>
+        
+                  <div class="parallax-container">
+                    <div class="parallax"><img src="images/image1.jpg"></div>
+                  </div>
+                 <div class="section white">
+                    <div class="row container">
+                      <h2 class="header">Parallax</h2>
+                      <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+                      <a class="waves-effect waves-light btn center-align" id="btnUsers">Usuarios</a>
+                    </div>
+                  </div>
+                  <div class="parallax-container">
+                    <div class="parallax"><img src="images/image4.jpg"></div>
+                  </div>
     </body>
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/init.js"></script>
+    <script>
+        $(document).ready(function(){
+          $('.parallax').parallax();
+        });
+           
+        $('#btnUsers').click(function(){
+            window.location.replace("usuario");
+        });
+    </script>
 </html>
